d2eec418bf0c0e4c8fc6ba09f10bd7cfec1d12bd
Kearney <191615342@qq.com>
2021-01-25 10:35:22 +0800

    添加debug info和修改了一处代码

--- README.md
+++ README.md
@@ -1,4 +1,35 @@
 # code_to_flowchart
 TThe program will take as input - code, algorithm, workflow or manual and will give as output- a flowchart for it.
 The code is written in python,I am using pygame for displaying flowchart.
-first flowchart.py with some program as input frr eg: "python3 flowchart.py bubble_sort.py t.flow" , then python3 python3 viszulaizer.py t.flow
+first flowchart.py with some program as input frr eg: `python flowchart.py for.py t.flow` , then `python visualizer.py t.flow`
+## example
+```bash
+> python flowchart.py examples/codes/for.py t.flow
+print("List Iteration")
+[<class 'node.OutputNode'>( "List Iteration") to None]
+l = ["geeks", "for", "geeks"]
+[<class 'node.OutputNode'>( "List Iteration") to <class 'node.Node'>(l = ["geeks", "for", "geeks"]) to None, <class 'node.Node'>(l = ["geeks", "for", "geeks"]) to None]
+for i in l:
+block node
+[<class 'node.OutputNode'>( "List Iteration") to <class 'node.Node'>(l = ["geeks", "for", "geeks"]) to None, <class 'node.Node'>(l = ["geeks", "for", "geeks"]) to None, <class 'node.ConnectorNode'>() to None]
+~print(i)
+[<class 'node.OutputNode'>( "List Iteration") to <class 'node.Node'>(l = ["geeks", "for", "geeks"]) to None, <class 'node.Node'>(l = ["geeks", "for", "geeks"]) to None, <class 'node.ConnectorNode'>() to <class 'node.Node'>(~print(i)) to None, <class 'node.Node'>(~print(i)) to None]
+
+
+> python visualizer.py t.flow
+pygame 2.0.1 (SDL 2.0.14, Python 3.9.0)
+Hello from the pygame community. https://www.pygame.org/contribute.html
+OverflowError: Python int too large to convert to C long
+
+The above exception was the direct cause of the following exception:
+
+Traceback (most recent call last):
+  File "D:\Documents\CAU\Lion\repositiries\Python\code_to_flowchart\visualizer.py", line 85, in <module>
+    draw(begin, SCREEN, 100, 100)
+  File "D:\Documents\CAU\Lion\repositiries\Python\code_to_flowchart\visualizer.py", line 48, in draw
+    pygame.draw.ellipse(
+SystemError: <built-in function ellipse> returned a result with an error set
+```
+# Note
+1. 安装最新版node模块会报错，不安装能正常出结果
+2. 第二步的时候报错说int型的值太大了，还有些pygame的bug，后面有空再修改吧。
--- visualizer.py
+++ visualizer.py
@@ -3,7 +3,6 @@ import pickle
 from sys import argv
 from node import *
 
-
 ## CONSTANTS ##
 SCREEN_SIZE = (800, 800)
 NODE_SIZE = (130, 50)
@@ -20,13 +19,18 @@ NODEW, NODEH = NODE_SIZE
 pygame.font.init()
 FONT = pygame.font.Font(None, 25)
 
+
 def rhombus(rect):
     x, y, w, h = tuple(map(int, rect))
-    return [[x-3*w//4, y], [x, y-h//2], [x+3*w//4, y], [x, y+h//2]]
+    return [[x - 3 * w // 4, y], [x, y - h // 2], [x + 3 * w // 4, y],
+            [x, y + h // 2]]
+
 
 def parallelogram(rect):
     x, y, w, h = tuple(map(int, rect))
-    return [[x-3*w//4, y+h//2], [x-w//2, y-h//2], [x+3*w//4, y-h//2], [x+w//2, y+h//2]]
+    return [[x - 3 * w // 4, y + h // 2], [x - w // 2, y - h // 2],
+            [x + 3 * w // 4, y - h // 2], [x + w // 2, y + h // 2]]
+
 
 def connect(screen, obj, pos, npos):
     if id(obj) in drawn:
@@ -35,30 +39,36 @@ def connect(screen, obj, pos, npos):
         pygame.draw.line(screen, LINE_COLOR, pos, npos, 5)
         draw(obj, screen, *npos)
 
+
 def draw(obj, screen, x, y):
     if not isinstance(obj, Node):
         raise Exception("I don't know how to draw %s" % str(obj))
 
     if type(obj) in (StartNode, StopNode):
-        pygame.draw.ellipse(screen, BLUE, pygame.Rect(x-NODEW/2, y-NODEH/2, NODEW, NODEH))
+        pygame.draw.ellipse(
+            screen, BLUE,
+            pygame.Rect(x - NODEW / 2, y - NODEH / 2, NODEW, NODEH))
     elif type(obj) is Node:
-        pygame.draw.rect(screen, NODE_COLOR, pygame.Rect(x-NODEW/2, y-NODEH/2, NODEW, NODEH))
+        pygame.draw.rect(
+            screen, NODE_COLOR,
+            pygame.Rect(x - NODEW / 2, y - NODEH / 2, NODEW, NODEH))
     elif type(obj) in (InputNode, OutputNode):
-        pygame.draw.polygon(screen, NODE_COLOR, parallelogram((x, y, NODEW, NODEH)))
+        pygame.draw.polygon(screen, NODE_COLOR,
+                            parallelogram((x, y, NODEW, NODEH)))
     elif type(obj) is ConnectorNode:
-        pygame.draw.circle(screen, NODE_COLOR, (x,y), NODEH//2)
+        pygame.draw.circle(screen, NODE_COLOR, (x, y), NODEH // 2)
     elif type(obj) is ConditionalNode:
-        pygame.draw.polygon(screen, GREEN, rhombus((x,y,NODEW,NODEH)))
-        drawn[id(obj)] = (x, y-NODEH)
-        connect(screen, obj.yes, (x, y), (x+NODE_HSPACE+NODEW, y))
-        connect(screen, obj.no, (x, y), (x, y+NODE_VSPACE+NODEH))
-    drawn[id(obj)] = (x, y-NODEH)
+        pygame.draw.polygon(screen, GREEN, rhombus((x, y, NODEW, NODEH)))
+        drawn[id(obj)] = (x, y - NODEH)
+        connect(screen, obj.yes, (x, y), (x + NODE_HSPACE + NODEW, y))
+        connect(screen, obj.no, (x, y), (x, y + NODE_VSPACE + NODEH))
+    drawn[id(obj)] = (x, y - NODEH)
     if obj.next:
-        connect(screen, obj.next, (x, y), (x, y+NODE_VSPACE+NODEH))
+        connect(screen, obj.next, (x, y), (x, y + NODE_VSPACE + NODEH))
 
     text = FONT.render(obj.text, 1, (255, 0, 0))
-    screen.blit(text, (x-NODEW//4,y-NODEH//4))
-        
+    screen.blit(text, (x - NODEW // 4, y - NODEH // 4))
+
 
 drawn = dict()
 
@@ -67,17 +77,15 @@ if len(argv) < 2:
     raise SystemExit()
 begin = pickle.load(open(argv[1], "rb"))
 
-
-assert pygame.init() == (6, 0)
-
-SCREEN = pygame.display.set_mode((800, 800),pygame.RESIZABLE)
+# assert pygame.init() == (6, 0)
+pygame.init()
+SCREEN = pygame.display.set_mode((800, 800), pygame.RESIZABLE)
 pygame.display.set_caption("Flowchart")
 SCREEN.fill((0, 0, 0))
 draw(begin, SCREEN, 100, 100)
 
 pygame.display.flip()
 while True:
-    event = pygame.event.poll()
+    event = pygame.event.get()
     if event.type == pygame.QUIT:
         break
-
